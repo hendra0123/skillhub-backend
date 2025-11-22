4be090c4e922c07927ecf75fbb0d0e1c9a9d4489
hendra0123 <nerstuff24@gmail.com>
2025-11-22 22:08:26 +0800

    feat: enable CORS for local development

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,            // buang field yang tidak ada di DTO
-      forbidNonWhitelisted: true, // tolak kalau ada field asing
-      transform: true,            // auto-cast string ke number di Param, dll
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
